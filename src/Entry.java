ac35004d88ca37471e7f9b20861202ecfb79042c
Mr. Behl <tbehl@student.neumont.edu>
2016-07-20 14:14:03 -0600

    Initial commit, contains method stubs

---
+++ src/Entry.java
@@ -0,0 +1,34 @@
+
+public class Entry {
+	
+	private String string;
+
+    private long value;
+
+    private long link;
+
+    public Entry(String string, long value, long link){
+    	
+    }
+
+    public String getString(){
+    	
+    }
+
+    public long getValue(){
+    	
+    }
+
+    public void setValue(long value){
+    	
+    }
+
+    public long getLink(){
+    	
+    }
+
+    public void setLink(long link){
+    	
+    }
+    
+}
