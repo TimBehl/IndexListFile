26eead0e23309881a0a08970e7b509e88631ef6b
TimBehl <timinaterb@gmail.com>
2016-07-27 14:13:03 -0600

    Filled out Entry method stubs

--- src/Entry.java
+++ src/Entry.java
@@ -8,27 +8,29 @@ public class Entry {
     private long link;
 
     public Entry(String string, long value, long link){
-    	
+    	this.string = string;
+    	this.value = value;
+    	this.link = link;
     }
 
     public String getString(){
-    	
+    	return this.string;
     }
 
     public long getValue(){
-    	
+    	return this.value;
     }
 
     public void setValue(long value){
-    	
+    	this.value = value;
     }
 
     public long getLink(){
-    	
+    	return this.link;
     }
 
     public void setLink(long link){
-    	
+    	this.link = link;
     }
     
 }
